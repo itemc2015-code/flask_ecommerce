bc7533fda5f238b71bbf8796dba6142ae2341fae
mark <it.emc2015@gmail.com>
2026-04-29 15:29:12 +0800

    add product yml for product list

--- product.py
+++ product.py
@@ -1,12 +1,15 @@
 from  flask import Blueprint,jsonify,render_template,current_app
+from flasgger import swag_from
 
 product_blueprint = Blueprint('product',__name__)
 
 @product_blueprint.route('/product_list',methods=['GET'])
+@swag_from('docs/product.yml')
 def product_lists():
     view_product = current_app.config['list_of_product']
     try:
         view_lists = view_product.product_list()
-        return render_template('main.html',view_lists=view_lists)
+        return view_lists
+        # return render_template('main.html',view_lists=view_lists)
     except:
         return jsonify({'message':'empty'})
